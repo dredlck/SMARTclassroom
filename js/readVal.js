ea89fe67efdddcd68a435c0de5112a55fdb2f03f
dredlck <45531016+dredlck@users.noreply.github.com>
2018-12-03 21:02:00 +0800

    Update readVal.js

--- js/readVal.js
+++ js/readVal.js
@@ -13,7 +13,7 @@
 
 	function loop() {
 		if (cpf) {
-			tempValue = DHT11();
+			tempValue = cpf.get("d3");;
 			document.getElementById("tempValue").innerHTML = tempValue;
 			smokeValue = cpf.get("a2");	
 			smokeValue1 = (1023 - smokeValue) / 26;
@@ -24,24 +24,12 @@
 			document.getElementById("noiseValue").innerHTML = noiseValue2;
 		}
 		if (tempValue != null && smokeValue != nuul && noiseValue != null) {
-			callPHP('?temp=' + tempValue + '&smk=' + smokeValue + '&noise=' + noiseValue)
+			var URL = "innobbatus.000webhostapp.com/sendsors/insert.php?temp=" + tempValue + "&smk=" + smokeValue + "&noise=" + noiseValue;
+			xmlhttp.open("GET",URL,true);
 		}
 		setTimeout("loop()", 1000);
 	}
 	
-	function callPHP(params) {
-    		var httpc = new XMLHttpRequest(); // simplified for clarity
-    		var url = "innobbatus.000webhostapp.com/sendsors/insert.php";
-    		httpc.open("POST", url, true); // sending as POSTt
-
-    		httpc.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    		httpc.onreadystatechange = function() { //Call a function when the state changes.
-       	 	if(httpc.readyState == 4 && httpc.status == 200) { // complete and no errors
-           		alert(httpc.responseText); // some processing here, or whatever you want to do with the response
-        	}
-    		};
-    		httpc.send(params);
-	}
 
 
 	// cpf setup
@@ -49,10 +37,4 @@
 		if(cpf)
 			cpf.setPinMode('["resetPin"],["setPinMode", "analog", 2, "INPUT"],["setPinMode", "analog", 1, "INPUT"],["setPinMode", "digital", 3,"INPUT"]');
 		
-	}
-
-	function DHT11() {
-		tempValue = cpf.get("DHT11","D3","dht11temp");
-// 		ui.set("D3","dht11temp",DHT11GetVal);
-// 		return DHT11GetVal;
 	}
