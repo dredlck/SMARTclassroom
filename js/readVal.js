18c901886eb8d2466b84e86414a1ee2739787bbc
dredlck <45531016+dredlck@users.noreply.github.com>
2018-12-03 13:43:49 +0800

    Update readVal.js

--- js/readVal.js
+++ js/readVal.js
@@ -2,7 +2,10 @@
 	
 	var tempValue;
 	var smokeValue;
+	var smokeValue1;
 	var noiseValue;
+	var noiseValue1;
+	var noiseValue2;
 	
 	loop();
 
@@ -11,12 +14,12 @@
 			tempValue = cpf.get("d3");
 			document.getElementById("tempValue").innerHTML = tempValue;
 			smokeValue = cpf.get("a2");	
-			smokeValue = (1023 - smokeValue) / 26;
-			document.getElementById("smokeValue").innerHTML = smokeValue;
+			smokeValue1 = (1023 - smokeValue) / 26;
+			document.getElementById("smokeValue").innerHTML = smokeValue1;
 			noiseValue = cpf.get("a1");
-			noiseValue = (noiseValue * 5) / 1023;
-			noiseValue = (noiseValue * 32);
-			document.getElementById("noiseValue").innerHTML = noiseValue;
+			noiseValue1 = (noiseValue * 5) / 1023;
+			noiseValue2 = (noiseValue1 * 32);
+			document.getElementById("noiseValue").innerHTML = noiseValue2;
 		}
 // 		if (tempValue != 0 && smokeValue != 0 && noiseValue != 0) {
 // 			callPHP('temp=' + tempValue + '&smk=' + smokeValue + '&noise=' + noiseValue)
