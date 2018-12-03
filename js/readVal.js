ac898ac90fe0e4a638e407b70eccec5479d73ea0
dredlck <45531016+dredlck@users.noreply.github.com>
2018-12-04 01:29:54 +0800

    Update readVal.js

--- js/readVal.js
+++ js/readVal.js
@@ -15,11 +15,11 @@
 			presValue = cpf.get("d4");
 			document.getElementById("tempValue").innerHTML = presValue;
 			smokeValue = cpf.get("a2");	
-			smokeValue1 = (1023 - smokeValue) / 26;
+			smokeValue1 = ((1023 - smokeValue) / 26).toFixed(2);
 			document.getElementById("smokeValue").innerHTML = smokeValue1;
 			noiseValue = cpf.get("a1");
 			noiseValue1 = (noiseValue * 5) / 1023;
-			noiseValue2 = (noiseValue1 * 32);
+			noiseValue2 = (noiseValue1 * 32).toFixed(2);
 			document.getElementById("noiseValue").innerHTML = noiseValue2;
 		}
 		if (tempValue != null && smokeValue != null && noiseValue != null) {
