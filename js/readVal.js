31311f850f09ee742f6039acde483273b2627c7e
dredlck <45531016+dredlck@users.noreply.github.com>
2018-12-04 10:25:13 +0800

    Update readVal.js

--- js/readVal.js
+++ js/readVal.js
@@ -42,7 +42,7 @@ function httpGetAsync(theUrl, callback)
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+//             callback(xmlHttp.responseText);
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
